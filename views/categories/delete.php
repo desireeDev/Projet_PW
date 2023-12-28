b45841ea05c7b215ea4cba3e0fa093cee4ad2cf9
Desiree <synticheattoh01@gmail.com>
2023-12-28 01:13:17 +0100

    Crud partie CategorieTerminé

--- views/categories/delete.php
+++ views/categories/delete.php
@@ -1,3 +1,30 @@
+
+<?php
+
+
+    require_once('../../config/config.php');
+    require_once('../../classes/models/Connexion.php');
+    require_once('../../classes/models/Categories.php');
+    require_once('../../classes/dao/CategoriesDAO.php');
+
+    $code = isset($_REQUEST['id']) ? $_REQUEST['id'] : '' ;
+
+    if(empty($code)){
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
+    
+    $dao = new CategoriesDAO(new Connexion());
+
+    $current_line = $dao->getByCode($code) ;
+
+    if($current_line == null){
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -13,10 +40,10 @@
 
     <form action="../../controllers/CategorieController.php"  method="post" >
         <label for="libelle">NomCategorie :</label>
-        <input type="text" id="libelle" name="libelle" required><br>
+        <input type="text" id="libelle" value="<?=$current_line->getCat()?>" name="libelle" required><br>
 
         <label for="code">codeRaccourci:</label>
-        <input type="text" id="code" name="code" required><br>
+        <input type="text" id="code" name="code" value="<?=$current_line->getCodeRaccourci()?>" required><br>
 
         <input type="submit" name="action" value="Supprimer">
     </form>

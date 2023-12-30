87c2677de29a4703c43b53248074af111441b825
Desiree <synticheattoh01@gmail.com>
2023-12-30 22:43:27 +0100

    Refonte des interfaces

--- views/contact/create.php
+++ views/contact/create.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Ajouter un contact</title>
-    <!-- Ajoutez ici vos liens CSS ou styles pour la mise en forme -->
-        <link rel="stylesheet" href="../css/styles.css">
-   
-</head>
-<body>
-    <h1>Ajouter un  contact</h1>
-    <a href="../../controllers/HomeController.php"> Liste des differentes contacts  </a>
- 
 
-    <form action="../../controllers/ContactController.php "  method="post" >
-        <label for="id">Numero du contact :</label>
-        <input type="text" id="id" name="id" required><br>
-
-        <label for="numLicencie">Numero du Licencie :</label>
-        <input type="text" id="numLicencie" name="numLicencie" required><br>
-        
-        <label for="nom">Nom du Contact :</label>
-        <input type="text" id="nom" name="nom" required><br>
-        <label for="prenom">Prenom du Contact :</label>
-        <input type="text" id="prenom" name="prenom" required><br>
-        <label for="email">Email du Contact :</label>
-        <input type="text" id="email" name="email" required><br>
-        <label for="telephone">Telephone du contact :</label>
-        <input type="text" id="telephone" name="telephone" required><br>
-
-        <input type="submit" name="action" value="Ajouter">
-    </form>
-
- 
     <!DOCTYPE html>
 <html>
 	<head>
@@ -45,8 +12,8 @@
 	</head>
 
 	<body>
-    <h1>Ajouter un  contact</h1>
-    <a href="../../controllers/HomeController.php"> Liste des differentes contacts  </a>
+
+    <a href="../../controllers/HomeController.php"> Liste des differents contacts  </a>
 
 		<div class="wrapper">
 			<div class="inner">
@@ -54,27 +21,39 @@
 				<form action="../../controllers/ContactController.php "  method="post">
 					<h3>Ajout d'un contact?</h3>
 					<div class="form-holder">
-						<span class="lnr lnr-key"></span>
-						<input type="text" class="form-control"  id="id" name="id" required placeholder="Numero du Licencie">
+						<span class="lnr lnr-user"></span>
+						<input type="text" class="form-control"  id="id" name="id" required placeholder="Numero du contact">
 					</div>
 					<div class="form-holder">
 						<span class="lnr lnr-phone-handset"></span>
-						<input type="text" class="form-control" placeholder="Phone Number">
+						<input type="text" class="form-control"  id="numLicencie" name="numLicencie" required placeholder="Numéro du Licencié">
 					</div>
+
 					<div class="form-holder">
+						<span class="lnr lnr-user"></span>
+						<input type="text" class="form-control" id="nom" name="nom" required  placeholder="Nom du Contact ">
+					</div>
+
+                    <div class="form-holder">
+						<span class="lnr lnr-user"></span>
+						<input type="text" class="form-control" id="prenom" name="prenom" required  placeholder="Prenom du Contact ">
+					</div>
+
+                    <div class="form-holder">
 						<span class="lnr lnr-envelope"></span>
-						<input type="text" class="form-control" placeholder="Mail">
+						<input type="text" class="form-control"id="email" name="email" required placeholder="Email du Contact ">
 					</div>
-					<div class="form-holder">
-						<span class="lnr lnr-lock"></span>
-						<input type="password" class="form-control" placeholder="Password">
+
+                    <div class="form-holder">
+						<span class="lnr lnr-phone"></span>
+						<input type="text" class="form-control"id="telephone" name="telephone" required placeholder="Telephone du Contact ">
 					</div>
-					<div class="form-holder">
-						<span class="lnr lnr-lock"></span>
-						<input type="password" class="form-control" placeholder="Confirm Password">
-					</div>
+
+
+				
 					<button>
-						<span>Register</span>
+					
+                    <input type="submit" name="action" value="Ajouter">
 					</button>
 				</form>
 				<img src="../../images/image-2.png" alt="" class="image-2">
